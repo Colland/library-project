3b15be7fb14286bf66454c7e8dda06b1ccaf84c5
Colland <t.jack.mac@gmail.com>
2022-02-08 20:58:51 +1300

    Fully functional

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 
 let myLibrary = [];
 
-function Book(author, title, numPages, finished)
+function Book(author, title, numPages, finished, bookID)
 {
     this.author = author;
     this.title = title;
     this.numPages = numPages;
     this.finished = finished;
+    this.bookID = bookID;
 }
 
 const adderButtton = document.querySelector("#adder");
 const modalDisplay = document.querySelector("#bookModal");
 const overlay = document.querySelector("#overlay");
 const submit = document.querySelector('#submit')
+
+const modalTitle = document.querySelector("#title");
+const modalAuthor = document.querySelector("#author");
+const modalPages = document.querySelector("#pages");
+const modalFinished = document.querySelector("#finishedCheckBox");
 
 adderButtton.addEventListener('click', () => {
     modalDisplay.classList.add("active");
@@ -30,7 +36,24 @@ window.addEventListener('mousedown', (event) => {
     }
 })
 
-submit.addEventListener('click', addBookToLibrary)
+submit.addEventListener('click', verifySubmit)
+
+function verifySubmit()
+{
+    if(modalTitle.value == '')
+    {
+    }
+    else if(modalAuthor.value == '')
+    {
+    }
+    else if(modalPages.value == '')
+    {
+    }
+    else
+    {
+        addBookToLibrary()
+    }
+}
 
 function addBookToLibrary()
 {
@@ -40,6 +63,7 @@ function addBookToLibrary()
     book.author = document.querySelector("#author").value;
     book.numPages = document.querySelector("#pages").value;
     book.finished = document.querySelector("#finishedCheckBox").checked;
+    book.bookID = "id" + Math.random().toString(16).slice(2);
     myLibrary.push(book);
 
     document.querySelector("#title").value = '';
@@ -65,22 +89,54 @@ function displayBookCard(book)
 
     author.textContent = book.author;
     title.textContent = book.title;
-    pages.textContent = book.numPages;
+    pages.textContent = book.numPages + " pages";
     remove.textContent = "Remove";
 
     if(book.finished)
     {
-        read.textContent = "read";
+        read.textContent = "Read";
+        read.style.backgroundColor = "#63ff63";
     }
     else
     {
-        read.textContent = "not read";
+        read.textContent = "Not read";
+        read.style.backgroundColor = "#ff6464";
     }
 
-    bookCard.classList.add("bookCard")
+    bookCard.classList.add("bookCard");
+    author.classList.add("cardLabel");
+    title.classList.add("cardLabel");
+    pages.classList.add("cardLabel");
+    remove.classList.add("cardButton");
+    read.classList.add("cardButton");
 
-    bookCard.appendChild(author);
+    bookCard.dataset.bookID = book.bookID;
+
+    remove.addEventListener('click', (e) => {
+        e.target.parentNode.remove();
+        myLibrary.splice(myLibrary.indexOf(book), 1);
+    })
+
+    read.addEventListener('click', (e) => {
+        let arrayBuffer = myLibrary.filter(book => book.bookID == e.target.parentNode.dataset.bookID);
+        let currentBook = arrayBuffer[0];
+        
+        if(currentBook.finished == true)
+        {
+            e.target.textContent = "Not read";
+            e.target.style.backgroundColor = "#ff6464";
+        }
+        else
+        {
+            e.target.textContent = "Read";
+            e.target.style.backgroundColor = "#63ff63"
+        }
+
+        currentBook.finished = !currentBook.finished;
+    })
+
     bookCard.appendChild(title);
+    bookCard.appendChild(author);
     bookCard.appendChild(pages);
     bookCard.appendChild(read);
     bookCard.appendChild(remove);
